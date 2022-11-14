575fe2968a3c802aa3af81400b7a2cc4a53712ac
DESKTOP-CEHRP21\kimda <nguyenkimdat2307@gmail.com>
2022-11-14 09:48:57 +0700

    danh muc san pham

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\DB;
 class PagesController extends Controller
 {
     public function index() {
-        return view('client.index');
+        $cate_product = DB::table('category')->orderBy('CategoryID','DESC')->get();
+        
+        return view('client.index')->with('category',$cate_product);
     }
 }
