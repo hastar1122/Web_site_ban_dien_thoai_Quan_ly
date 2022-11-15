2ada0cebf5d97c4a574588b30939507ebe7dac89
Nguyen2 <Ngo2uHuy@gmail.com>
2022-11-15 23:16:41 +0700

    Danh sách sản phẩm

--- resources/views/client/layouts/navigation.blade.php
+++ resources/views/client/layouts/navigation.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="agileits-navi_search">
-                <form action="#" method="post">
+                <form action="{{ URL::to('/list-products') }}" method="post">
                     <select id="agileinfo-nav_search" name="agileinfo_search" class="border" required="">
                         <option value="">Danh mục sản phẩm</option>
                         @foreach ($category as $key => $cate)
@@ -67,7 +67,9 @@
                             </div>
                         </div>
                     </li>
-                   
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ URL::to('/list-products') }}">Danh sách sản phẩm</a>
+                    </li>
                     <li class="nav-item dropdown mr-lg-2 mb-lg-0 mb-2">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Giới thiệu
